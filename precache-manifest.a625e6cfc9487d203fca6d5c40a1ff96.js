37656b2d30776a06c6bb1358e5ac6d0f6d7e393d
shadaakshari <shadaakshari15@gmail.com>
2022-08-11 15:38:17 +0530

    Updates

---
+++ precache-manifest.a625e6cfc9487d203fca6d5c40a1ff96.js
@@ -0,0 +1,82 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51f4042f11add9e762ac0da0691253d8",
+    "url": "/shadaakshari-portfolio/index.html"
+  },
+  {
+    "revision": "b005f6d054470e922e63",
+    "url": "/shadaakshari-portfolio/static/css/2.e4777ce8.chunk.css"
+  },
+  {
+    "revision": "d1291d63381e700d9aa0",
+    "url": "/shadaakshari-portfolio/static/css/main.c7222efe.chunk.css"
+  },
+  {
+    "revision": "b005f6d054470e922e63",
+    "url": "/shadaakshari-portfolio/static/js/2.08bd68fc.chunk.js"
+  },
+  {
+    "revision": "00c55dacee0f8a3af6b1e69fb15f3717",
+    "url": "/shadaakshari-portfolio/static/js/2.08bd68fc.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d1291d63381e700d9aa0",
+    "url": "/shadaakshari-portfolio/static/js/main.1a382355.chunk.js"
+  },
+  {
+    "revision": "cee12b500050527c0f05",
+    "url": "/shadaakshari-portfolio/static/js/runtime-main.0e85491c.js"
+  },
+  {
+    "revision": "6dad6444c0788a46d8ccf2e2915f079b",
+    "url": "/shadaakshari-portfolio/static/media/amazon.6dad6444.png"
+  },
+  {
+    "revision": "d2138f86a8d461777f1bcaaf07ab7e57",
+    "url": "/shadaakshari-portfolio/static/media/cb_quiz.d2138f86.png"
+  },
+  {
+    "revision": "a3015f4b7a1a844d29c8fa45f49e62fa",
+    "url": "/shadaakshari-portfolio/static/media/data_viz.a3015f4b.png"
+  },
+  {
+    "revision": "a756e0112873e52212b583f157860a26",
+    "url": "/shadaakshari-portfolio/static/media/developer.a756e011.jpeg"
+  },
+  {
+    "revision": "4be813cee0ad0bc4a8df08cd3a6e1fe4",
+    "url": "/shadaakshari-portfolio/static/media/django_blog.4be813ce.png"
+  },
+  {
+    "revision": "19029594964f204b6ee642735ca725b7",
+    "url": "/shadaakshari-portfolio/static/media/fefit.19029594.png"
+  },
+  {
+    "revision": "285babe4b3e41acb6b09a52762d00df3",
+    "url": "/shadaakshari-portfolio/static/media/fencing_scoreboard.285babe4.png"
+  },
+  {
+    "revision": "282dcf2e08ad64e8bd19fcc4c3722c5b",
+    "url": "/shadaakshari-portfolio/static/media/json-table.282dcf2e.png"
+  },
+  {
+    "revision": "bf4d336b8d28daa2a7bdd2030e44182b",
+    "url": "/shadaakshari-portfolio/static/media/mobile-portfolio.bf4d336b.jpg"
+  },
+  {
+    "revision": "be70127d6573a04f7545a3aa7031a706",
+    "url": "/shadaakshari-portfolio/static/media/phishfgen.be70127d.png"
+  },
+  {
+    "revision": "949bd3ee1e73e5acead460a2fc6ed547",
+    "url": "/shadaakshari-portfolio/static/media/portfolio.949bd3ee.png"
+  },
+  {
+    "revision": "f50236716682567003c1480f2b75b27f",
+    "url": "/shadaakshari-portfolio/static/media/portfolioshaddu.f5023671.png"
+  },
+  {
+    "revision": "56b94ec4f60f7fe38e45707cc58acdc9",
+    "url": "/shadaakshari-portfolio/static/media/whiteboard.56b94ec4.png"
+  }
+]);
