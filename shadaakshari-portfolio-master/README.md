5da4bccbbbc5a51d15ad076735f8ef761e6c11e5
shadaakshari <shadaakshari15@gmail.com>
2022-11-03 22:10:49 +0530

    initial commit

---
+++ shadaakshari-portfolio-master/README.md
@@ -0,0 +1,11 @@
+# Portfolio
+
+A mobile-responsive portfolio scaffold. The live version can be found here:https://shadaakshari-kalagara.github.io/shadaakshari-portfolio/
+
+Built using React.js.
+Deployed on Github
+
+
+
+
+
---
+++ shadaakshari-portfolio-master/src/data.js
@@ -0,0 +1,123 @@
+const displayName = 'Shadaakshari Kalagara'
+const bannerDescription = 'Enthusiastic Student Developer and eternal learner'
+const mediumUrl = ''
+const mediumFilterKeyword = 'technology'
+const email = 'shadaakshari19@gmail.com'
+const githubUrl = "https://github.com/shadaakshari-Kalagara"
+const linkedInUrl = "https://www.linkedin.com/in/shadaakshari-kalagara-47a9781b4/"
+
+
+const projects = [
+    {
+        title: 'PhishGen',
+        description: 'A phishing detection web application that helps the user for safe browsing',
+        stack: ['Python', 'Jupyter', 'Flask', 'Heroku'],
+        imgName: 'phishfgen.png',
+        siteUrl: 'http://cdcproject1.herokuapp.com/',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/PHISHING-WEBSITE-DETECTION',
+    },
+    {
+        title: 'Amazon E-commerce analytics',
+        description: 'The full model encompassing all product categories was developed to give significant variables that affected ranking of products',
+        stack: [ 'Python', 'Jupyter', 'R'],
+        imgName: 'amazon.png',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/Amazon-product-ranking-analytics',
+        altCodeUrl: 'https://github.com/huishun98/songdedi-requests'
+    },
+    {
+        title: 'FeFit',
+        description: 'a Fitness tracker exclusively for women to keep a tab of their fitness journey.',
+        stack: ['HTML', 'CSS', 'JavaScript', 'React JS', 'NodeJs'],
+        imgName: 'fefit.png',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/FeFit-fitness-website.github.io',
+    },
+    {
+        title: 'Stock Market prediction of Nifty 50',
+        description: 'Analyzing, predicting and visualizing the data of fifteen NIFTY 50 companies of four sectors: Finance , Automobile , IT , goods',
+        stack: ['R', 'Python'],
+        imgName: 'StockMarket.png',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/Stock-market-prediction',
+    },
+    {
+        title: 'Personal Portfolio',
+        description: 'This website gives an insight into the projects (personal/academic) that I have developed .',
+        stack: ['React JS', 'HTML', 'CSS', 'JavaScript'],
+        imgName: 'portfolioshaddu.png',
+        siteUrl: 'https://shadaakshari-kalagara.github.io/shadaakshari-portfolio/',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/shadaakshari-Kalagara.github.io',
+    },
+    {
+        title: 'Collaborative white board',
+        description: 'An online doodling white board that can be used and by many users simultaneously.',
+        stack: ['Node JS', 'React JS', 'HTML', 'CSS', 'JavaScript'],
+        imgName: 'whiteboard.png',
+        siteUrl: '',
+        codeUrl: 'https://github.com/shadaakshari-Kalagara/white-board-collaborative',
+    },
+    
+]
+
+const articles = [
+    {
+        stack: ['C++'],
+        imgName: 'c++.png',
+    },
+
+    {
+        stack: ['JAVA'],
+        imgName: 'java.png',
+    },
+    {
+        stack: ['PYTHON'],
+        imgName: 'python.png',
+    },
+    {
+        stack: ['SQL'],
+        imgName: 'sql.png',
+    },
+    {
+        stack: ['MySQL'],
+        imgName: 'mysql.jpg',
+    },
+    {
+        stack: ['JavaScript'],
+        imgName: 'js.png',
+    },
+    {
+        stack: ['HTML'],
+        imgName: 'html.png',
+    },
+    {
+        stack: ['CSS'],
+        imgName: 'css.png',
+    },
+    {
+        stack: ['React JS'],
+        imgName: 'react.png',
+    },
+    {
+        stack: ['Flask'],
+        imgName: 'flask.jpg',
+    },
+    {
+        stack: ['Django'],
+        imgName: 'django.png',
+    },
+    {
+        stack: ['Node JS'],
+        imgName: 'node.jpg',
+    },
+    {
+        stack: ['Git'],
+        imgName: 'git.png',
+    },
+    {
+        stack: ['R'],
+        imgName: 'r.png',
+    },
+    
+
+
+]
+
+export { projects,articles, mediumUrl, mediumFilterKeyword, email, githubUrl, linkedInUrl, displayName, bannerDescription }
